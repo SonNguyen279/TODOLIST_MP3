ee691be821b2ad78271038e855c850d769b65388
SonNguyen279 <nguyenvansonln1102@gmail.com>
2024-10-01 17:02:04 +0700

    v3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import { useState } from "react";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   const [ work, setWork ] = useState ('')
+  const [ todos, setTodos ] = useState([])
+
+  const handleAdd = () => {
+    if (todos?.some(item => item.id === work?.replace(/\s/g, ''))) {
+      toast.warn('Công việc đã được thêm vào trước đó')
+    } else {
+      setTodos(prev => [...prev, { id: work?.replace(/\s/g, ''), job: work }])
+      setWork('')
+    }
+  }
+
+  const handleDeleteJob = (id) => {
+    setTodos(prev => prev.filter(item => item.id !== id))
+  }
 
   return (
-    <div className="flex gap-8 h-screen items-center border border-red-500 justify-center">
+   <>
+    <div className="flex flex-col gap-8 h-screen items-center border border-red-500 justify-center">
+      <div className="flex gap-8">
       <input 
       type="text"
       className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
@@ -15,10 +33,39 @@ function App() {
       <button
       type="button"
       className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+      onClick={handleAdd}
       >
-
+        Add
       </button>
+      </div>
+
+      <div>
+        <h3 className="font-bold text-x1">Content</h3>
+        <ul>
+          {todos?.map((item, index) => {
+            return (
+              <li key={index.id} className="flex gap-10 items-center">
+                <span className="my-2">{item.job}</span>
+                <span onClick={() => handleDeleteJob(item.id)} className="my-2 cursor-pointer p-2">X</span>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
+    <ToastContainer
+    position="top-right"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="light"
+    />
+   </>
   );
 }
 
