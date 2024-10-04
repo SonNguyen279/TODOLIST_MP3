d8ac6a1f4ff87d409c9affd40a18ac9548009786
SonNguyen279 <nguyenvansonln1102@gmail.com>
2024-10-04 19:36:52 +0700

    Thiet lap react router dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { Home, Login, Public } from "./containers/public/";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Routes, Route } from "react-router-dom";
+import path from "./ultis/path";
 
 function App() {
-  const { test, homeData } = useSelector(state => state.app)
 
   return (
    <>
     <div className="">
-      app
+      <Routes>
+        <Route path={path.PUBLIC} element={<Public />}>
+          <Route path={path.HOME} element={<Home />} />
+          <Route path={path.LOGIN} element={<Login />} />
+        </Route>
+      </Routes>
     </div>
     <ToastContainer
     position="top-right"
